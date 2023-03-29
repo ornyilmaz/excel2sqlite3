bbff3f681296d00c2994924caf81bc8e09262e6d
ORHAN YILMAZ <ornyilmaz@gmail.com>
2023-03-29 22:43:09 +0300

    Update README.m

--- README.m
+++ README.m
@@ -1,3 +1,5 @@
 Excel sayfalarını tablo ve ilk satırı da sutun anlamı olarak ayrıştırarak sqlite3 database forma çevirir.
 
 AI desteği ile taslağı oluşturulup düzenlenmiştir.
+
+!(excel2sqlite.JPG)
